1d3fdbc495449f8d6c6065dc781573a7e5a1db7a
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-04-17 17:55:15 +0300

    14th commit. Task 024. Difference of Arrays (arr1 - arr2)

--- src/main/java/temp.java
+++ src/main/java/temp.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class temp {
     public static void main(String[] args) {
 
@@ -8,11 +10,19 @@ public class temp {
 //        String newName = myName.substring(0,4)+'x'+myName.substring(2);
 //        System.out.println(newName);
 
-        String s = "abc";
-        String x = "";
-        x = x + s.charAt(2);
-        System.out.println(x);
+//        String s = "abc";
+//        String x = "";
+//        x = x + s.charAt(2);
+//        System.out.println(x);
 
+                            //    |
+        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80};
+        System.out.println("WAS: " + Arrays.toString(arr));
+        int ind_del = 3;
+        int[] result = new int[arr.length - 1];
+
+        System.arraycopy(arr, 4, arr, 3,arr.length - ind_del - 1);
+        System.out.println("NOW: " + Arrays.toString(arr));
 
 
 
